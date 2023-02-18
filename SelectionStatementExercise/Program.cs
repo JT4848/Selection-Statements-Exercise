0a3de3828cd5788f4c01c73c6230c86aa22e15de
John Thomas Pricone <jtpricone@gmail.com>
2023-02-18 15:01:21 -0500

    done again

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace SelectionStatementExercise
+﻿using System.Security.Cryptography;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Can you guess my favorite number?");
+            var r = new Random();
+            var favNumber = r.Next(48);
+            var userInput = int.Parse(Console.ReadLine());
+
+            
+
+            if(userInput < 48)
+            {
+                Console.WriteLine("To Low");
+            }
+            else if(userInput > 48)
+            {
+                Console.WriteLine("To High");
+            }
+            else
+            {
+                Console.WriteLine("You Got It");
+            }
         }
     }
 }
